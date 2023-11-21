6da844e0707469542c7e21d1fd6dcdf273c730db
Sofia <SofiGuchkova@gmail.com>
2023-11-22 00:04:46 +0100

    Hello Test

--- src/main/java/org/telran/pro/algorythms/hw/hw3/Fibonach.java
+++ src/main/java/org/telran/pro/algorythms/hw/hw3/Fibonach.java
@@ -1,4 +1,37 @@
 package org.telran.pro.algorythms.hw.hw3;
 
+
 public class Fibonach {
+
+
+//Переменная для вовда рекурсивного метода
+private final int n = 20;
+
+    public int getN() {
+
+        return n;
+    }
+
+    // Рекурсивная
+    int  getFibRec (int n){
+        if(n <= 1) {
+            return n;
+        }
+        return getFibRec(n-1) + getFibRec(n-2);
+    }
+    // Итеративная
+    void getFibIte(int n){
+        int i = 2;
+        int [] arrFib = new int[n];
+        while(i<n){
+            arrFib[1] = 1;
+            arrFib[0] = 0;
+            arrFib[i] = arrFib[i-1] + arrFib[i-2];
+            System.out.println(arrFib[i]);
+            i++;
+
+        }
+    }
+
 }
+
--- src/main/java/org/telran/pro/algorythms/hw/hw3/HwThreeApp.java
+++ src/main/java/org/telran/pro/algorythms/hw/hw3/HwThreeApp.java
@@ -1,7 +1,35 @@
 package org.telran.pro.algorythms.hw.hw3;
 
+//Ответ:
+//
 public class HwThreeApp {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Fibonach varFib = new Fibonach();
+        HanoiTower hanoiTower = new HanoiTower();
+
+        // Рекурсия
+
+        //Для 30 вызовов - ???
+        //Для 20 вызовов совершено 21896 операций. 1.65^n
+        //Для 10 вызовов совершено 179 операций = 1.67^n
+        //Вывод: Чем больше входных данных, тем дольше работает программа.
+        //Применять рекурсивный метод для вычисления Фибоначи очень затратно = много операций
+
+        for(int i = 0; i < varFib.getN(); i++) {
+            System.out.println("Число " + varFib.getFibRec(i));
+        }
+
+        // Итерация
+        // Прямая зависимость по времени O(n) . 30 вызовов = 30 итераций. 20 вызовов = 20 итераций.
+        // Вывод - Итеративный метод для вычисления чисел Фибоначи работает намного быстрее с большим количеством данных,
+        // чем рекурсивный
+
+          varFib.getFibIte(30);
+
+
+          // Ханойская башня
+        hanoiTower.HanoiTowerSolve(3,"First Tower ", "Third Tower ", "Second Tower ");
+
+        }
     }
-}
+
